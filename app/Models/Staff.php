4fc7a6c6f09e64dcf73680dbfc14356531cfed30
MarsWu <mars.wu@onramplab.com>
2022-03-02 16:42:17 +0800

    seeder: staff factory with its related table

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Department;
+use App\Models\LeaveApplication;
+use App\Models\StaffLeave;
 
 class Staff extends Model
 {
@@ -18,5 +20,15 @@ class Staff extends Model
     public function department()
     {
         return $this->hasOne(Department::class, 'id', 'department_id');
+    }
+
+    public function leaveBalance()
+    {
+        return $this->hasOne(StaffLeave::class, 'id', 'staff_id')->where('year', date('Y'));
+    }
+
+    public function leaveApplications()
+    {
+        return $this->hasMany(LeaveApplication::class, 'id', 'staff_id');
     }
 }
--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Staff;
+use App\Models\StaffLeave;
 use App\Models\LeaveApplication;
 use Illuminate\Database\Seeder;
 
@@ -16,16 +17,26 @@ class StaffSeeder extends Seeder
     public function run()
     {
         Staff::truncate();
+        StaffLeave::truncate();
+        LeaveApplication::truncate();
 
         // create a staff
         // 新增一名員工
         Staff::factory()
             ->create();
 
-        // create 3 staff
-        // 新增三名員工
+        // create 3 staff with staff leaves
+        // 新增三名員工和他們的可休假時數
         Staff::factory()
             ->count(3)
+            ->has(
+                StaffLeave::factory()
+                    ->count(1)
+                    ->state(function (array $attributes, Staff $staff) {
+                        return ['staff_id' => $staff->id];
+                    }),
+                'leaveBalance',
+            )
             ->create();
 
         // create a staff who is still in the probation period and named John.
@@ -33,7 +44,38 @@ class StaffSeeder extends Seeder
         Staff::factory([
             'name' => 'John',
         ])
+        ->has(
+            StaffLeave::factory()
+                ->count(1)
+                ->inProbationPeriod()
+                ->state(function (array $attributes, Staff $staff) {
+                    return ['staff_id' => $staff->id];
+                }),
+            'leaveBalance',
+        )
         ->inProbationPeriod()
         ->create();
+
+        // create a staff with staff leaves and a leave application form
+        // 新增一個員工，和他的可休假時數與一張請假單
+        Staff::factory()
+            ->count(1)
+            ->has(
+                StaffLeave::factory()
+                    ->count(1)
+                    ->state(function (array $attributes, Staff $staff) {
+                        return ['staff_id' => $staff->id];
+                    }),
+                'leaveBalance',
+            )
+            ->has(
+                LeaveApplication::factory()
+                    ->count(1)
+                    ->state(function (array $attributes, Staff $staff) {
+                        return ['staff_id' => $staff->id];
+                    }),
+                'leaveApplications',
+            )
+            ->create();
     }
 }
