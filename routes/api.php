2826503a81e9a07e215d710b9a14d868f906f2a8
MarsWu <mars.wu@onramplab.com>
2022-04-30 13:24:29 +0800

    test-B

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::get('/test-B', function () {
+    return view('welcome');
+});
